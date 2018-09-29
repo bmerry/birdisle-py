8da193f91d79b18cb6fc49c43a72a9eff9973b04
Bruce Merry <bmerry@gmail.com>
2018-09-29 12:00:47 +0200

    Add unit tests for Lua code and blocking operation
    Closes #4, #5

--- tests/test_birdisle.py
+++ tests/test_birdisle.py
@@ -1,3 +1,5 @@
+import time
+import threading
 import locale
 
 import redis
@@ -5,14 +7,17 @@ import birdisle
 import pytest
 
 
-def test_simple():
-    r = birdisle.StrictRedis()
+@pytest.fixture
+def r():
+    return birdisle.StrictRedis()
+
+
+def test_simple(r):
     r.set('hello', 'world')
     assert r.get('hello') == b'world'
 
 
-def test_repr():
-    r = birdisle.StrictRedis()
+def test_repr(r):
     repr(r)
 
 
@@ -29,3 +34,22 @@ def test_locale():
     assert r.get('foo') == b'1.75'
     # Check that birdisle hasn't overridden this thread's locale
     assert locale.format('%g', 1.5) == '1,5'
+
+
+def test_lua(r):
+    r.set('foo', 'bar')
+    script = r.register_script("return redis.call('GET', KEYS[1])")
+    result = script(['foo'])
+    assert result == b'bar'
+
+
+def test_blocking(r):
+    def worker(r):
+        time.sleep(0.1)
+        r.rpush('foo', 'bar')
+
+    thread = threading.Thread(target=worker, args=(r,))
+    thread.start()
+    result = r.blpop('foo')
+    assert result == (b'foo', b'bar')
+    thread.join()
