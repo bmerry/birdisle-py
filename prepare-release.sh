96b7a6a5a649952b5d08d3bd17e322ad3d1b166b
Bruce Merry <bmerry@gmail.com>
2021-09-02 15:40:30 +0200

    Update prepare_release.sh for Github Actions
    Artifacts are now built by Actions

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,20 +4,14 @@
 # - Update birdisle/_version.py
 # - Update doc/changelog.rst
 # - Run this script
+# - Push to Github
+# - Download sdist and wheels from Github Actions
 # - Upload the source dist and wheels to PyPI
 # - Create a git tag and push it
 
 set -e -u
 
-python - <<EOF
-import sys
-if sys.version_info[0] != 3:
-    raise RuntimeError("Run in a Python 3 environment")
-EOF
-pip install setuptools_git
 make -C doc clean
 make -C doc html
 flake8
 pytest --forked
-./setup.py sdist
-./mkwheels.sh
